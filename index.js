76a913efe333fc5ceaa259f36e93813fb9efb63d
Andrey Burdey <nrgnevskyfrontak@gmail.com>
2021-02-02 23:11:20 +0500

    ?

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import App from './App';
 import {name as appName} from './app.json';
 import {Provider} from 'react-redux';
 import configureStore from './src/store';
+import 'react-native-gesture-handler';
+
 const store = configureStore();
 
 const reduxStore = () => 
