cd8403fa8c28094777b690d5a3affc0ce7c9f152
vladimir-skikh <19vladimir99@gmail.com>
2021-02-13 20:40:25 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,9 +1,6 @@
 let Config = {
     'API': 'https://lelerestapi.na4u.ru/module/lelerestapi',
-<<<<<<< HEAD
-=======
     'baseURI': 'https://lelerestapi.na4u.ru/',
->>>>>>> orderhistory
     'currency': '£',
 }
 export default Config;
